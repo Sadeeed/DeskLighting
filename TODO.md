d2af36ebe9a9aec6f67ccdf5b93f567bd228b0f0
Sadeeed <sadeeedw@gmail.com>
2021-07-23 20:53:36 +0500

    :tada: Initial Commit

---
+++ TODO.md
@@ -0,0 +1,13 @@
+# TODO
+
++ Have separate sliders for Hue(H), Saturation(S) and Value(V)
+
+  + Attach an identifier character at the end to distinguish b/w different values Eg. 14H would be a value of 14 for Hue
+
++ Allow baudrate customization (possibly through Menu bar)
+
++ COM Port Selection
+
++ Custom / Default Presets (Menu Bar)
+
++ Slider value is only sent when a button is pressed
---
+++ src/desklighting.py
@@ -0,0 +1,49 @@
+import serial
+import time
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+def write_read(x):
+    mcuSerial.reset_input_buffer()
+    mcuSerial.write(bytes(x, 'utf-8'))
+    data = mcuSerial.readline()
+    return data
+
+class Handler:
+    def onDestroy(self, *args):
+        Gtk.main_quit()
+
+    def onHueChange(self, *args):
+        hue = str(int(hueSlider.get_value())) + "H"
+        print(hue)
+        # write_read(hue)
+        print(write_read(hue))
+
+    def onSaturationChange(self, *args):
+        saturation = str(int(saturationSlider.get_value())) + "S"
+        print(saturation)
+        # write_read(saturation)
+        print(write_read(saturation))
+
+    def onValueChange(self, *args):
+        value = str(int(valueSlider.get_value())) + "V"
+        print(value)
+        # write_read(value)
+        print(write_read(value))
+
+
+mcuSerial = serial.Serial(port="/dev/ttyACM1", baudrate=9600, timeout=0.5)
+# load glade UI xml
+builder = Gtk.Builder()
+uiFile = "src/ui/LightApp.glade"
+builder.add_from_file(uiFile)
+builder.connect_signals(Handler())
+window = builder.get_object("MainWindow")
+
+hueSlider = builder.get_object("hueSlider")
+saturationSlider = builder.get_object("saturationSlider")
+valueSlider = builder.get_object("valueSlider")
+
+window.show_all()
+Gtk.main()
